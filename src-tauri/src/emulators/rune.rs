def5dd95ba6a72966d25269b38b237058cd69647
Serigne <serigne.ibrahim.thiam@gmail.com>
2026-04-05 23:23:49 +0200

    feat: add Rune INI parser skeleton

--- src-tauri/src/emulators/rune.rs
+++ src-tauri/src/emulators/rune.rs
@@ -2,6 +2,7 @@
 
 use std::path::PathBuf;
 use std::collections::HashMap;
+use ini::Ini;
 use serde::{Deserialize};
 use crate::achievements::models::Achievement;
 use crate::emulators::EmulatorParser;
@@ -10,6 +11,12 @@ pub struct Parser;
 
 impl EmulatorParser for Parser {
     fn parse(&self, path: &PathBuf) -> Vec<Achievement> {
+        let achievementfile = Ini::load_from_file(path).unwrap();
+        let section = achievementfile.section(Some("SteamAchievements")).unwrap();
+        let mut index: Vec<String>  = Vec::new();
+        for (_key, value) in section.iter() {
+            index.push(value.to_string());
+        }
         todo!()
     }
 }
