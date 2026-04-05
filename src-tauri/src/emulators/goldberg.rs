17b2dd1f44135c9402fd12a0579de2d0fe1edb76
Serigne <serigne.ibrahim.thiam@gmail.com>
2026-04-05 18:56:32 +0200

    feat: add Goldberg parser skeleton

--- src-tauri/src/emulators/goldberg.rs
+++ src-tauri/src/emulators/goldberg.rs
@@ -1,1 +1,17 @@
+use std::path::PathBuf;
 // goldberg: parse le format JSON de Goldberg
+use serde::{Deserialize};
+use crate::achievements::models::Achievement;
+use crate::emulators::EmulatorParser;
+
+#[derive(Deserialize)]
+pub struct Parser {
+    pub earned : bool,
+    pub earned_time : Option<u64>,
+}
+
+impl EmulatorParser for Parser {
+    fn parse(&self, path: &PathBuf) -> Vec<Achievement> {
+        todo!()
+    }
+}
